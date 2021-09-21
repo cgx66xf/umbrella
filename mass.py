91f2af4ddb19ca82a259e7b70580100cb9b87b74
Taner Paker <taner.2001@hotmail.com>
2021-09-21 16:16:43 -0400

    Update mass.py
    added main function

--- mass.py
+++ mass.py
@@ -2,28 +2,34 @@ import subprocess
 import random
 import re
 
-def random_ip():	
-	random1= random.randint(0, 255)
-	random2= random.randint(0, 255)
-	random3= random.randint(0, 255)
-	random4= random.randint(0, 255)
-	randomip= ('{}.{}.{}.{}').format(random1,random2,random3,random4)
-	return randomip
+#this file outputs a random online host.
 
-def scan_ip():
-	masscan= subprocess.run(('timeout 1 ping -c1 {}').format(random_ip()), shell= True, capture_output= True)
-	masscan= masscan.stdout.decode()
-	return(masscan) 
+def main():
 
-def grep():
-	pattern= re.compile(r'(-{3})(\s)(\d+\.\d+\.\d+\.\d+)')
-	matches= pattern.findall(scan_ip())
-	for match in matches:
-		return match[2]
+	def random_ip():	
+		random1= random.randint(0, 255)
+		random2= random.randint(0, 255)
+		random3= random.randint(0, 255)
+		random4= random.randint(0, 255)
+		randomip= ('{}.{}.{}.{}').format(random1,random2,random3,random4)
+		return randomip
+
+	def scan_ip():
+		masscan= subprocess.run(('timeout 1 ping -c1 {}').format(random_ip()), shell= True, capture_output= True)
+		masscan= masscan.stdout.decode()
+		return(masscan) 
+
+	def grep():
+		pattern= re.compile(r'(-{3})(\s)(\d+\.\d+\.\d+\.\d+)')
+		matches= pattern.findall(scan_ip())
+		for match in matches:
+			return match[2]
 
 
-while True:
-	output= grep()
-	if output != None:
-		print(output)
-		break
+	while True:
+		output= grep()
+		if output != None:
+			print(output)
+			break
+
+main()
