5c37f479fc72c75b3fea8d8d74770d48d186a4c5
Taner Paker <taner.2001@hotmail.com>
2021-09-27 15:41:02 -0400

    partially implemented parse_matches()

--- main.py
+++ main.py
@@ -31,15 +31,86 @@ def target():
 		else:
 			print("arguments are weird, check --help")
 
+
 def nmap():  #this returns the open ports against a target
 	enemy= "scanme.nmap.org"
 	process= subprocess.run('nmap {}'.format(enemy), shell= True, capture_output= True)
 	result= (process.stdout.decode())
 	pattern= re.compile(r'(\d+)\/(tcp|udp)\s+(open|filtered)\s+(\S+)')
 	matches= pattern.findall(result)
-	return (matches)
-	
-nmap()
+	return matches
 
-	
+def parse_matches():
+	for i in matches:
+		if (i[0] == '80'):
+			#http_scan()
+			print("http0")
+
+		elif (i[0] == '23'):
+			#telnet_scan()
+			print("telnet0")
+		
+		elif (i[0] == '443'):
+			#https_scan()
+			print("https0")
+
+		elif (i[0] == '21'):
+			#ftp_scan()
+			print("ftp0")
+
+		elif (i[0] == '22'):
+			#ssh_scan()
+			print("ssh0")
+
+		elif (i[0] == '25'):
+			#smtp_scan()
+			print("smtp0")
+		
+		elif (i[0] == '3389'):
+			#ms-wbt-server_scan()
+			print("ms-wbt-server0")
+
+		elif (i[0] == '110'):
+			#pop3()
+			print("pop30")
+
+		elif (i[0] == '445'):
+			#smb_scan()
+			print("smb0")
+
+		elif (i[0] == '139'):
+			#netbios-ssn_scan()
+			print("netbios-ssn0")
+
+		elif (i[0] == '143'):
+			#imap_scan()
+			print("imap0")
+
+		elif (i[0] == '53'):
+			#dns_scan()
+			print("dns0")
+		
+		elif (i[0] == '135'):
+			#msrpc_scan()
+			print("msrpc0")
+
+		elif (i[0] == '3306'):
+			#mysql_scan()
+			print("mysql0")
+
+		elif (i[0] == '8080'):
+			#http-proxy_scan()
+			print("http-proxy0")
+
+		elif (i[0] == '1723'):
+			#pptp_scan()
+			print("pptp0")
+
+		elif (i[0] == '111'):
+			#rpcbind_scan()
+			print("rpcbind0")
+		
+		else:
+			print("unknown port:",i[0])
+
 		
